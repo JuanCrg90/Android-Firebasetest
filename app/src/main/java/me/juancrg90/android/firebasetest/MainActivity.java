7f5f17508cd9674627e8a62cc514c20262b13244
Juan Carlos Ruiz <JuanCrg90@gmail.com>
2016-06-06 22:19:47 -0500

    Added Firebase database listener

--- app/src/main/java/me/juancrg90/android/firebasetest/MainActivity.java
+++ app/src/main/java/me/juancrg90/android/firebasetest/MainActivity.java
@@ -4,9 +4,13 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -29,6 +33,25 @@ public class MainActivity extends AppCompatActivity {
 
         database = FirebaseDatabase.getInstance();
         myRef = database.getReference("message");
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d("Debug", "Value is: " + value);
+                Toast.makeText(MainActivity.this, value, Toast.LENGTH_SHORT).show();
+
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w("Warning", "Failed to read value.", error.toException());
+            }
+        });
     }
 
     @OnClick(R.id.button)
@@ -36,4 +59,6 @@ public class MainActivity extends AppCompatActivity {
         myRef.setValue(editText.getText().toString());
         editText.setText("");
     }
+
+
 }
